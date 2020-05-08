552ad603c452eeeccc1ea0bb262799cf05f4f100
Nurcahya Nania Anabela <nania.anabela22@gmail.com>
2020-05-08 19:27:25 +0700

    config firebase

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCSka0CzivBFIJqiSrme5CgjMtr3FP5KQ0",
+    authDomain: "projectreactjs-uas.firebaseapp.com",
+    databaseURL: "https://projectreactjs-uas.firebaseio.com",
+    projectId: "projectreactjs-uas",
+    storageBucket: "projectreactjs-uas.appspot.com",
+    messagingSenderId: "527672161799",
+    appId: "1:527672161799:web:37d9060016cf33c11d9ca2"
+  };
+
+  export default firebaseConfig;
