fd19b09d9b201b1f38b31663267ad9f014254d38
Nurcahya Nania Anabela <nania.anabela22@gmail.com>
2020-05-08 19:27:10 +0700

    contact user

---
+++ src/Contact.js
@@ -0,0 +1,19 @@
+// Step 6
+import React, { Component } from "react";
+
+class Contact extends Component {
+    render() {
+      return (
+        <div>
+          <center><h3>Contact Person</h3></center>
+          <center><h5>===================================================</h5></center>
+          <center><h8>For more information :</h8></center>
+          <center><h8>Nurcahya Nania Anabela</h8></center>
+          <center><h8>Jl. Taman Bunga Merak 1 No. 48</h8></center>
+          <center><h8>085449321928</h8></center>
+        </div>
+      );
+    }
+  }
+   
+  export default Contact;
