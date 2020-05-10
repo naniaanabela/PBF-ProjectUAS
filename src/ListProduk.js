aefe27f091528e10d888b47c23f6a99bb36705c7
Nurcahya Nania Anabela <nania.anabela22@gmail.com>
2020-05-10 22:44:56 +0700

    fix list product scripts

--- src/ListProduk.js
+++ src/ListProduk.js
@@ -2,21 +2,20 @@ import React from "react";
 
 const ListProduk = (props) => {
     return (
-        <div className="produk">
-            {/* <div className="gambar-artikel">
-                <img src="http://placeimg.com/80/80/tech" alt="Gambar Tumbnail Artikel"/>
-            </div> */}
-            <div className="konten-artikel">
-                <div className="judul-produk">{props.judul}</div>
-                <p><img src={props.image} width="50px"/></p>
-                <p className="price-produk">{props.price}</p>
-                <p className="isi-produk">{props.isi}</p>
-                <button className="btn btn-sm btn-warning" 
+        <div class="container">     
+            <table class="table table-striped">
+                <tbody>
+                    <tr>
+                    <td>{props.judul}</td>
+                    <td>{props.price}</td>
+                    <td>{props.isi}</td>
+                    <td><img src={props.image} width="50px"/></td>
+                    <td><button className="btn btn-sm btn-warning" 
                     onClick={() => { if (window.confirm('Apakah anda yakin menghapus produk ini?')) 
-                    props.hapusProduk(props.idProduk)}} > 
-                    Hapus
-                </button>
-            </div>
+                    props.hapusProduk(props.idProduk)}} > Hapus</button></td>
+                    </tr>
+                    </tbody>
+            </table>
         </div>
     )
 }
